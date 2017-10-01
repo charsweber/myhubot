a54fa78832c2c30c48c84914c23b49db240f8a3c
Charlie <weberscharles@gmail.com>
2017-10-01 13:24:01 -0500

    commit it

--- scripts/bender.js
+++ scripts/bender.js
@@ -1,10 +1,10 @@
 // hubot scripts:
 module.exports = function(bender) {
-	bender.hear(/Hello!|hi/, function(res) {
+	bender.hear(/Hello!/, function(res) {
 	  return res.reply("Hi there!");
 	});
 
-	bender.respond(/Who's on first?/, function(res) {
-	  return res.send("What's on second?");
-	});
+	// bender.respond(/Who's on first?/, function(res) {
+	//   return res.send("What's on second?");
+	// });
 }
