e58fbee2fcd39ccebb6b554df2638d3954560a82
Charlie <weberscharles@gmail.com>
2017-10-01 13:16:17 -0500

    first commit

---
+++ scripts/bender.js
@@ -0,0 +1,10 @@
+// hubot scripts:
+module.exports = function(bender) {
+	bender.hear(/Hello!|hi/, function(res) {
+	  return res.reply("Hi there!");
+	});
+
+	bender.respond(/Who's on first?/, function(res) {
+	  return res.send("What's on second?");
+	});
+}
